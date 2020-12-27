018e9bd50996627723847b01c0c8f0453859f6fe
NickAc <32451103+NickAcPT@users.noreply.github.com>
2020-12-27 12:56:58 +0000

    Remove all files

--- NBrigadier/SingleRedirectModifier.cs
+++
@@ -1,9 +0,0 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT license.
-
-using NBrigadier.Context;
-
-namespace NBrigadier
-{
-    public delegate TS SingleRedirectModifier<TS>(CommandContext<TS> context);
-}
